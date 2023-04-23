3aaacfd146936995deaae73dd24993e997f91dea
Jordan Maxwell <jordan@strangeloopgames.com>
2023-04-23 17:47:43 -0500

    Added documentation

--- droid/notify.py
+++ droid/notify.py
@@ -1,4 +1,11 @@
 """
+This module provides classes for processing and handling notifications from a connected droid. 
+
+It contains the DroidNotifyMessage class, which represents a notification message from the droid, and 
+the DroidNotificationProcessor class, which handles incoming notifications from the droid and passes 
+them to the appropriate handlers. The DroidNotificationProcessor class also includes methods for decoding 
+incoming messages and verifying firmware versions.
+
 This class is licensed under MIT.
 """
 
@@ -9,10 +16,26 @@ from droid.hardware import DroidFirmwareVersion
 
 class DroidNotifyMessage(object):
     """
+    Represents a notification message received from a droid.
+
+    Attributes:
+        message_size (int): The size of the message.
+        unknown1 (int): An unknown integer value.
+        command_id (int): The ID of the command associated with the notification.
+        unknown3 (int): Another unknown integer value.
+        message_data (str): The data associated with the notification.
     """
 
     def __init__(self, message_size: int, unknown1: int, command_id: int, unknown3: int, message_data: str):
         """
+        Initializes a new instance of the DroidNotifyMessage class.
+
+        Args:
+            message_size (int): The size of the message.
+            unknown1 (int): An unknown integer value.
+            command_id (int): The ID of the command associated with the notification.
+            unknown3 (int): Another unknown integer value.
+            message_data (str): The data associated with the notification.
         """
 
         self.message_size = message_size
@@ -23,6 +46,10 @@ class DroidNotifyMessage(object):
 
     def __str__(self) -> str:
         """
+        Returns a string representation of the DroidNotifyMessage instance.
+
+        Returns:
+            str: A string representation of the DroidNotifyMessage instance.
         """
 
         return ('Size: %s, Unknown1: %s, Command Id: %s, Unknown2: %s, Data: %s' % (
@@ -30,17 +57,31 @@ class DroidNotifyMessage(object):
 
 class DroidNotificationProcessor(object):
     """
+    Processes notification events from a connected droid.
+
+    Attributes:
+        droid (object): The connected droid instance.
     """
 
     def __init__(self, droid: object) -> None:
         """
+        Initializes a new instance of the DroidNotificationProcessor class.
+
+        Args:
+            droid (object): The connected droid instance.
         """
 
         self.droid = droid
 
     def decode_notify_message(self, data: bytearray) -> DroidNotifyMessage:
         """
-        Decodes the incoming message to a DroidNotifyMessage instance
+        Decodes an incoming message to a DroidNotifyMessage instance.
+
+        Args:
+            data (bytearray): The incoming message data.
+
+        Returns:
+            DroidNotifyMessage: A DroidNotifyMessage instance representing the incoming message.
         """
 
         hex_data = data.hex()
@@ -57,7 +98,11 @@ class DroidNotificationProcessor(object):
 
     async def handle_incoming_message(self, sender: object, data: bytearray) -> None:
         """
-        Processes notification events from the connected droid
+        Processes notification events from the connected droid.
+
+        Args:
+            sender (object): The sender of the notification event.
+            data (bytearray): The notification event data.
         """
 
         try:
