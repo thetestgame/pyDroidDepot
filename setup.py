70f5562253d26672dac9fbcc8a9880a74ab30c7b
Jordan Maxwell <jordan@strangeloopgames.com>
2023-04-25 02:02:27 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ Module for controlling droids built and purchased at the Droid Depot in Disney's
 """
 
 setup(
-    name='pydroid',
+    name='pyDroidDepot',
     description="Module for controlling droids built and purchased at the Droid Depot in Disney's Galaxys Edge",
     long_description=long_description,
     license='MIT',
-    version='1.0.0',
+    version='1.0.1',
     author='Jordan Maxwell',
     maintainer='Jordan Maxwell',
-    url='https://github.com/thetestgame/pydroid',
-    packages=['droid'],
+    url='https://github.com/thetestgame/pyDroidDepot',
+    packages=['droiddepot'],
     classifiers=[
         'Programming Language :: Python :: 3',
     ])
